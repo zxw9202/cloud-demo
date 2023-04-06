c0aaed915a5edd45084b1720ae272627762dc829
zhangxiaowan <18354276865@163.com>
2023-04-06 16:22:15 +0800

    服务集成nacos

--- order-service/src/main/java/com/zxw/controller/OrderController.java
+++ order-service/src/main/java/com/zxw/controller/OrderController.java
@@ -4,17 +4,25 @@ import com.zxw.domain.Video;
 import com.zxw.domain.VideoOrder;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/order")
 @Api(value="订单服务")
+@Slf4j
 public class OrderController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Autowired
     RestTemplate restTemplate;
@@ -22,7 +30,12 @@ public class OrderController {
     @RequestMapping("/save")
     @ApiOperation("保存视频订单")
     public Object save(int videoId){
-        Video video = restTemplate.getForObject("http://localhost:9000/api/v1/video/findById?videoId="+videoId, Video.class);
+        List<ServiceInstance> list = discoveryClient.getInstances("video-service");
+        ServiceInstance serviceInstance = list.get(0);
+        String url = "http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+
+                "/api/v1/video/findById?videoId="+videoId;
+        log.info("url:【{}】",url);
+        Video video = restTemplate.getForObject(url, Video.class);
         VideoOrder videoOrder = new VideoOrder();
         videoOrder.setVideoId(video.getId());
         videoOrder.setVideoTitle(video.getTitle());
--- video-service/src/main/java/com/zxw/VideoApplication.java
+++ video-service/src/main/java/com/zxw/VideoApplication.java
@@ -3,11 +3,13 @@ package com.zxw;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.zxw.dao")
 @EnableSwagger2
+@EnableDiscoveryClient
 public class VideoApplication {
 
     public static void main(String[] args){
